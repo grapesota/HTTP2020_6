ef46f47b23d9dfdc615d7d89188613f3463c45ba
グレープ粗茶 <48442343+grapesota@users.noreply.github.com>
2020-06-15 12:03:55 +0900

    Update post.php

--- post.php
+++ post.php
@@ -2,3 +2,17 @@
 print("<h1>view post parametar:post</h1>\n");
 if(isset($_POST['post']))
     echo "post: ". htmlspecialchars($_POST['post'], ENT_QUOTES, 'UTF-8');
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+
+</head>
+<body>
+<form action="" metehod="post">
+    <input type="text" name="post">
+    <input type="submit" value="submit">
+</form>
+</body>
+</html>
