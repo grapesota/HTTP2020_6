61cbbcf1418acfd7cba66356c53a3fd8e96cdd5d
グレープ粗茶 <48442343+grapesota@users.noreply.github.com>
2020-06-16 23:43:42 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $dir = __DIR__;
 if (is_dir($dir)) {
     if ($dh = opendir($dir)) {
         while (($file = readdir($dh)) !== false) {
-            if($file!=="."&&$file!==".."&&$file!==".git"&&$file!=="index.php"&&$file!==".composer"&&$file!=="vendor"&&$file!==".heroku"&&$file!==".profile.d"&&$file!=="composer.json"&&$file!=="Procfile")
+            if($file!=="."&&$file!==".."&&$file!==".git"&&$file!=="index.php"&&$file!==".composer"&&$file!=="vendor"&&$file!==".heroku"&&$file!==".profile.d"&&$file!=="composer.json"&&$file!=="Procfile"&&$file!=="README.md")
                 echo "<a  href=\"$file\"> $file</a> " ."<br>";
         }
         closedir($dh);
