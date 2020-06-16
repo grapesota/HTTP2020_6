200413ce8bde2d9d931f661e1285e650ca5b3dbc
グレープ粗茶 <48442343+grapesota@users.noreply.github.com>
2020-06-16 23:45:25 +0900

    Update get.php

--- get.php
+++ get.php
@@ -2,6 +2,7 @@
 echo "<h1>view GET parametar:get </h1>\n";
 if(isset($_GET['get'])) 
     echo "get: ".htmlspecialchars($_GET['get'], ENT_QUOTES, 'UTF-8');
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
