6f6b13c4fc134d16f2f26bf9299e6c53098088a9
グレープ粗茶 <48442343+grapesota@users.noreply.github.com>
2020-07-11 19:55:53 +0900

    Update get.php

--- get.php
+++ get.php
@@ -1,14 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<body>    
 <?php
 echo "<h1>view GET parametar:get </h1>\n";
 if(isset($_GET['get'])) 
     echo "get: ".htmlspecialchars($_GET['get'], ENT_QUOTES, 'UTF-8');
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
 
-</head>
-<body>
+
 <form action="" method="get">
     <input type="text" name="get">
     <input type="submit" value="submit">
